6712c65a3a23ae7cbff1b7741edb71e43dec0cde
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-02 19:06:19 +0530

    dark mode added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import "./App.css";
 // import About_us from "./Components/About_us";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
   return (
     <>
-      <Navbar title="Text Utility App" />
-      <TextForm heading="Welcome to Text Utility App"/>
+      <Navbar title="Text Utility App" mode={mode} toggleMode={toggleMode} />
+      <TextForm heading="Welcome to Text Utility App" mode={mode} />
 
       {/* <About_us /> */}
     </>
